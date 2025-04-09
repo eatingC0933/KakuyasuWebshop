5c198fbe529aecb754291eafc72f0e42058ba8cc
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:10:30 +0200

    Update MvcConfig.java

--- src/main/java/se/yitingchang/testwebshop/Config/MvcConfig.java
+++ src/main/java/se/yitingchang/testwebshop/Config/MvcConfig.java
@@ -1,4 +1,38 @@
 package se.yitingchang.testwebshop.Config;
 
-public class MvcConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/").setViewName("user_login");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/user/register").setViewName("user_register");
+        registry.addViewController("/cart").setViewName("cart");
+        registry.addViewController("/orders").setViewName("orders");
+        registry.addViewController("/orderDetails").setViewName("orderDetails");
+        registry.addViewController("/addNewProduct").setViewName("addNewProductForm");
+        registry.addViewController("/category").setViewName("category");
+        registry.addViewController("/category-product").setViewName("category-product");
+        registry.addViewController("/order_confirmation").setViewName("order_confirmation");
+        registry.addViewController("/products").setViewName("products");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    }
+
+
+
 }
