32b53e899b9c8970e74461b64e2a453a8ec9a3df
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:24:44 +0200

    Update CategoryService.java

--- src/main/java/se/yitingchang/testwebshop/Service/CategoryService.java
+++ src/main/java/se/yitingchang/testwebshop/Service/CategoryService.java
@@ -1,4 +1,30 @@
 package se.yitingchang.testwebshop.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se.yitingchang.testwebshop.Repository.CategoryRepository;
+import se.yitingchang.testwebshop.model.Category;
+
+import java.util.List;
+
+
+@Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public void saveCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+   public Category getCategoryById(int categoryId) {
+        return  categoryRepository.findByCategoryId(categoryId);
+   }
+
 }
+
