7d0d05494a4b14f47f51ac84602ddbcc5ab02627
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:20:42 +0200

    Update CartRepository.java

--- src/main/java/se/yitingchang/testwebshop/Repository/CartRepository.java
+++ src/main/java/se/yitingchang/testwebshop/Repository/CartRepository.java
@@ -1,4 +1,21 @@
 package se.yitingchang.testwebshop.Repository;
 
-public interface CarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.yitingchang.testwebshop.model.Cart;
+import se.yitingchang.testwebshop.model.Product;
+import se.yitingchang.testwebshop.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+    List<Cart> findByUser(Optional<User> user);
+
+
+
+    Cart findByCartId(int cartId);
+
+    List<Cart> findByUser_Id(int userId);
 }
