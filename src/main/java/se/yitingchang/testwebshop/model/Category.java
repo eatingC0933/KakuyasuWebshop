b3b55fe60be63bbe261aab28995f8f9cf29f8cec
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:28:09 +0200

    Update Category.java

--- src/main/java/se/yitingchang/testwebshop/model/Category.java
+++ src/main/java/se/yitingchang/testwebshop/model/Category.java
@@ -1,4 +1,54 @@
 package se.yitingchang.testwebshop.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.List;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id",nullable = false)
+    private int categoryId;
+
+    @Column(name = "category_name",nullable = false)
+    private String categoryName;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Product> products;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
