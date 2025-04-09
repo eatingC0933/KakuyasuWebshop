140ab9c6b87d39ca73f54bea1de1769dd8d581ee
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:22:33 +0200

    Update ProductRepository.java

--- src/main/java/se/yitingchang/testwebshop/Repository/ProductRepository.java
+++ src/main/java/se/yitingchang/testwebshop/Repository/ProductRepository.java
@@ -1,4 +1,24 @@
 package se.yitingchang.testwebshop.Repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+import se.yitingchang.testwebshop.model.Category;
+import se.yitingchang.testwebshop.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+
+    List<Product> findByCategoryCategoryId(int categoryId);
+    Optional<Product> findById(int productId);
+    Optional<Product> findByProductName(String productName);
+    List<Product> findByProductNameContainingIgnoreCase(String keyword);
+
+
 }
