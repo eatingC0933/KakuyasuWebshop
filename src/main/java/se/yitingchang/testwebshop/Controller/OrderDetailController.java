f158a9601a7f9b824ce07fd050510abd832ad800
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:16:44 +0200

    Update OrderDetailController.java

--- src/main/java/se/yitingchang/testwebshop/Controller/OrderDetailController.java
+++ src/main/java/se/yitingchang/testwebshop/Controller/OrderDetailController.java
@@ -1,4 +1,30 @@
 package se.yitingchang.testwebshop.Controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import se.yitingchang.testwebshop.Service.OrderDetailService;
+import se.yitingchang.testwebshop.model.OrderDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/orderDetails")
 public class OrderDetailController {
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    @GetMapping("/{orderId}")
+    public String showOrderDetails(@PathVariable("orderId")int orderId, Model model) {
+        List<OrderDetails> orderDetails =  orderDetailService.getOrderDetailsByOrderId(orderId);
+        model.addAttribute("orderDetails", orderDetails);
+        return "orderDetail";
+    }
+
+ 
+
 }
+
