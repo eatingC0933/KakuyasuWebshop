3c783f093d4660711ad79e8974841a72fec26dae
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:21:44 +0200

    Update OrderDetailRepository.java

--- src/main/java/se/yitingchang/testwebshop/Repository/OrderDetailRepository.java
+++ src/main/java/se/yitingchang/testwebshop/Repository/OrderDetailRepository.java
@@ -1,4 +1,17 @@
 package se.yitingchang.testwebshop.Repository;
 
-public interface OrderDetailRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.yitingchang.testwebshop.model.OrderDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetails, Integer> {
+    List<OrderDetails> findByOrderOrderId(int orderId);
+
+    List<OrderDetails> findByOrderDetailId(int orderDetailId);
+
+
+
 }
