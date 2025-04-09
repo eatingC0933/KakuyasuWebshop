6c5cf60afa2774c6c439d52a7778c411468a8117
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:16:11 +0200

    Update OrderController.java

--- src/main/java/se/yitingchang/testwebshop/Controller/OrderController.java
+++ src/main/java/se/yitingchang/testwebshop/Controller/OrderController.java
@@ -1,4 +1,45 @@
 package se.yitingchang.testwebshop.Controller;
 
+
+import org.springframework.web.bind.annotation.*;
+import se.yitingchang.testwebshop.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import se.yitingchang.testwebshop.Service.OrderService;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/admin/orders")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public String orders(Model model) {
+        model.addAttribute("orders", orderService.getAllOrders());
+        return "orders";
+    }
+
+  
+    @GetMapping("/{orderID}")
+    public Order getOrderByID(@PathVariable int orderID) {
+        return  orderService.getOrderByOrderId(orderID);
+    }
+
+    @PostMapping
+    public String saveOrder(@ModelAttribute Order order) {
+        orderService.saveOrder(order);
+        return "redirect:/admin/orders";
+    }
+
+    @PostMapping("/updateStatus")
+    public String updateOrderStatus(@RequestParam("orderId") int orderId, @RequestParam("status") String status) {
+        orderService.updateOrderStatus(orderId, status);
+        return "redirect:/admin/orders";
+    }
 }
+
