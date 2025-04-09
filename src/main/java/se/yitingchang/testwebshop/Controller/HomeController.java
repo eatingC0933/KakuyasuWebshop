f0167ff819f54b69b6b6c43d4c530b63b26e10b4
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:15:34 +0200

    Update HomeController.java

--- src/main/java/se/yitingchang/testwebshop/Controller/HomeController.java
+++ src/main/java/se/yitingchang/testwebshop/Controller/HomeController.java
@@ -1,20 +1,71 @@
-package se.yitingchang.testwebshop;
+package se.yitingchang.testwebshop.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import se.yitingchang.testwebshop.Service.CategoryService;
+import se.yitingchang.testwebshop.Service.ProductService;
+import se.yitingchang.testwebshop.Service.UserService;
+import se.yitingchang.testwebshop.model.Category;
+import se.yitingchang.testwebshop.model.Product;
+import se.yitingchang.testwebshop.model.User;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private UserService userService;
+
+   @GetMapping("/")
     public String startPage() {
+
         return "user_login";
     }
 
-    @GetMapping("home")
-    public String home() {
+    @GetMapping("/home")
+    public String homePage(Model model, Principal principal) {
+        if (principal != null) {  // Check if the user is logged in
+            String username = principal.getName();
+            Optional<User> userOptional = userService.findByUserName(username);
+            model.addAttribute("user", userOptional.get());
+        }
+
+        List<Product> products = productService.getAllProducts();
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("products", products);
+        model.addAttribute("categories", categories);
+
         return "home";
     }
+
+    @GetMapping("/logout")
+    public String logoutPage() {
+        return "redirect:/user/login";
+    }
+
+  
+   @GetMapping("/home")
+    public String homePage(Model model, Principal principal) {
+    
+        List<Product> products = productService.getAllProducts();
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("products", products);
+        model.addAttribute("categories", categories);
+    }
+
 
 
 }
