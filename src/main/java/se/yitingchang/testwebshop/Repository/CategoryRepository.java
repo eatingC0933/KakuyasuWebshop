a74c50d25499cf626e5e8e5b5a7fa8a95af72cbd
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:21:18 +0200

    Update CategoryRepository.java

--- src/main/java/se/yitingchang/testwebshop/Repository/CategoryRepository.java
+++ src/main/java/se/yitingchang/testwebshop/Repository/CategoryRepository.java
@@ -1,4 +1,11 @@
 package se.yitingchang.testwebshop.Repository;
 
-public interface CategoryRepository {
+
+import se.yitingchang.testwebshop.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Category findByCategoryId(int categoryId);
 }
