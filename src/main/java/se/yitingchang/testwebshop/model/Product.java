22a49a190bf1bb8101f862e79949652cbd095d61
Yiting Chang <yiting.chang@iths.se>
2025-04-09 10:30:10 +0200

    Update Product.java

--- src/main/java/se/yitingchang/testwebshop/model/Product.java
+++ src/main/java/se/yitingchang/testwebshop/model/Product.java
@@ -1,4 +1,75 @@
 package se.yitingchang.testwebshop.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="product_id",nullable = false)
+    private int productId;
+
+    @Column(name="product_name",nullable = false)
+    private String productName;
+
+    @Column(name="price", nullable=false)
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name ="category_id", nullable = false)
+    private Category category;
+
+    @Column(name="image", nullable = false)
+    private String imagePath;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+
 }
